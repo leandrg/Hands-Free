b1cc4c76d170195ff3301166fa2268848643e329
leandr_g <gaetan.leandre@epitech.eu>
2020-02-05 16:14:29 +0100

    using -> replace strange format of function with something better :)

--- include/CommandParser.hpp
+++ include/CommandParser.hpp
@@ -9,6 +9,9 @@
 #include                <iostream>
 #include                <unordered_map>
 
+template<typename T>
+using ActionFunction = void (T::*)(std::string const &str);
+
 class                   CommandParser {
 
     class                   CommandParent {
@@ -22,10 +25,10 @@ class                   CommandParser {
     template <class T>
     class                   Command : public CommandParent {
         T                   &_context;
-        void (T::*_f)(std::string const &str);
+        ActionFunction<T>   _f;
 
     public:
-        Command(T &context, void (T::*f)(std::string const &str)) : _context(context), _f(f) {};
+        Command(T &context, ActionFunction<T> f) : _context(context), _f(f) {};
         ~Command(){};
         void                launch(std::string const &str) {
             (this->_context.*(this->_f))(str);
@@ -39,7 +42,7 @@ public:
     ~CommandParser();
 
     template <class T>
-            void addCommand(T &context, std::string key, void (T::*f)(std::string const &str)) {
+            void addCommand(T &context, std::string key, ActionFunction<T> f) {
                 Command<T> *tmp = new CommandParser::Command<T>(context, f);
                 this->_mapCommands[key] = tmp;
             }
