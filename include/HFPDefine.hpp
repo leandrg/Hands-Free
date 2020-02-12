75c1547179afbe37b914ecc6addf907ced22fd0a
leandr_g <gaetan.leandre@epitech.eu>
2020-02-12 11:30:10 +0100

    🐛change features #define format

--- include/HFPDefine.hpp
+++ include/HFPDefine.hpp
@@ -15,29 +15,29 @@
 #define             HFP_COMMAND_INDICATORS              "+CIND"
 
 //AT+BRSF FEATURES
-#define             HFP_AT_BRSF_FEATURE_RNEC            0
-#define             HFP_AT_BRSF_FEATURE_THREE_WAY_CALL  1
-#define             HFP_AT_BRSF_FEATURE_CLI             2
-#define             HFP_AT_BRSF_FEATURE_VOICE_RECO      3
-#define             HFP_AT_BRSF_FEATURE_REMOTE_VOL      4
-#define             HFP_AT_BRSF_FEATURE_CALL_STATUS     5
-#define             HFP_AT_BRSF_FEATURE_CALL_CONTROL    6
-#define             HFP_AT_BRSF_FEATURE_CODEC           7
-#define             HFP_AT_BRSF_FEATURE_HF_IND          8
-#define             HFP_AT_BRSF_FEATURE_ESCO            9
+#define             HFP_AT_BRSF_FEATURE_RNEC            (1 << 0)
+#define             HFP_AT_BRSF_FEATURE_THREE_WAY_CALL  (1 << 1)
+#define             HFP_AT_BRSF_FEATURE_CLI             (1 << 2)
+#define             HFP_AT_BRSF_FEATURE_VOICE_RECO      (1 << 3)
+#define             HFP_AT_BRSF_FEATURE_REMOTE_VOL      (1 << 4)
+#define             HFP_AT_BRSF_FEATURE_CALL_STATUS     (1 << 5)
+#define             HFP_AT_BRSF_FEATURE_CALL_CONTROL    (1 << 6)
+#define             HFP_AT_BRSF_FEATURE_CODEC           (1 << 7)
+#define             HFP_AT_BRSF_FEATURE_HF_IND          (1 << 8)
+#define             HFP_AT_BRSF_FEATURE_ESCO            (1 << 9)
 
 //+BRSF FEATURES
-#define             HFP_BRSF_FEATURE_THREE_WAY_CALL     0
-#define             HFP_BRSF_FEATURE_RNEC               1
-#define             HFP_BRSF_FEATURE_VOICE_RECO         2
-#define             HFP_BRSF_FEATURE_RING_TONE          3
-#define             HFP_BRSF_FEATURE_ATTACH_NUMBER      4
-#define             HFP_BRSF_FEATURE_REJECT_CALL        5
-#define             HFP_BRSF_FEATURE_CALL_STATUS        6
-#define             HFP_BRSF_FEATURE_CALL_CONTROL       7
-#define             HFP_BRSF_FEATURE_ERROR_RESULT       8
-#define             HFP_BRSF_FEATURE_CODEC              9
-#define             HFP_BRSF_FEATURE_HF_IND             10
-#define             HFP_BRSF_FEATURE_ESCO               11
+#define             HFP_BRSF_FEATURE_THREE_WAY_CALL     (1 << 0)
+#define             HFP_BRSF_FEATURE_RNEC               (1 << 1)
+#define             HFP_BRSF_FEATURE_VOICE_RECO         (1 << 2)
+#define             HFP_BRSF_FEATURE_RING_TONE          (1 << 3)
+#define             HFP_BRSF_FEATURE_ATTACH_NUMBER      (1 << 4)
+#define             HFP_BRSF_FEATURE_REJECT_CALL        (1 << 5)
+#define             HFP_BRSF_FEATURE_CALL_STATUS        (1 << 6)
+#define             HFP_BRSF_FEATURE_CALL_CONTROL       (1 << 7)
+#define             HFP_BRSF_FEATURE_ERROR_RESULT       (1 << 8)
+#define             HFP_BRSF_FEATURE_CODEC              (1 << 9)
+#define             HFP_BRSF_FEATURE_HF_IND             (1 << 10)
+#define             HFP_BRSF_FEATURE_ESCO               (1 << 11)
 
 #endif              //HANDS_FREE_HFPDEFINE_HPP
