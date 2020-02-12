fda8a8f110606cddec4728ddf48c571872e2a6c7
leandr_g <gaetan.leandre@epitech.eu>
2020-02-12 14:16:28 +0100

    🐛fix compil

--- source/ServerHFP.cpp
+++ source/ServerHFP.cpp
@@ -40,7 +40,7 @@ void                    ServerHFP::sendSupportedFeatures(ClientSocket *client, s
     client->onSuccess(*this, &ServerHFP::sendIndicatorsListQuestion); //TODO ADD CODECS?
 }
 
-void                    ServerHFP::sendIndicatorsListQuestion(ClientSocket *client, std::string const &question = "?") {
+void                    ServerHFP::sendIndicatorsListQuestion(ClientSocket *client, std::string const &question) {
     *client << HFP_COMMAND_SEND_BY_HF << HFP_COMMAND_INDICATORS << "=" << question << "\n";
     client->onSuccess(*this, &ServerHFP::sendIndicatorsListQuestion);
 }
