541f9f90c9c98715698f27a12563e07508925cb4
leandr_g <gaetan.leandre@epitech.eu>
2020-02-13 11:31:58 +0100

    ⚠️add print

--- source/ServerHFP.cpp
+++ source/ServerHFP.cpp
@@ -48,9 +48,7 @@ void                    ServerHFP::sendIndicatorsListQuestion(ClientSocket *clie
 
 void                    ServerHFP::sendIndicatorsValueQuestion(ClientSocket *client, std::string const &_) {
     (void)_;
-    *client << HFP_COMMAND_SEND_BY_HF << HPP_COMMAND_ERROR_CODE << "?" << "\n";
-
-//    *client << HFP_COMMAND_SEND_BY_HF << HFP_COMMAND_INDICATORS << "?" << "\n";
+    *client << HFP_COMMAND_SEND_BY_HF << HFP_COMMAND_INDICATORS << "?" << "\n";
     client->onSuccess(*this, &ServerHFP::sendEnableErrorMessage);
 }
 
