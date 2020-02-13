313b90829185b5c9a0135faa53224587047db1d6
leandr_g <gaetan.leandre@epitech.eu>
2020-02-13 10:58:10 +0100

    ⚠️add print

--- source/ServerHFP.cpp
+++ source/ServerHFP.cpp
@@ -104,7 +104,7 @@ void                    ServerHFP::receiveIndicatorsList(ClientSocket *client, s
         std::cout << vectorStr.size() << std::endl;
         std::vector<int> vectorInt;
         for (auto it = vectorStr.begin(); it != vectorStr.end(); it++) {
-            try {vectorInt.push_back(std::stoi(*it));} catch (...) {vectorInt.push_back(0); std::cout << "hgfdhjhgfd" << std::endl;}
+            try {vectorInt.push_back(std::stoi(*it));std::cout << *it << " " << std::stoi(*it) << std::endl;} catch (...) {vectorInt.push_back(0); std::cout << "hgfdhjhgfd" << std::endl;}
         }
     }
 }
