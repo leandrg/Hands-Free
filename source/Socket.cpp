c0fd0aac67940f21a1b09ccbf31d9795a7745f0d
leandr_g <gaetan.leandre@epitech.eu>
2020-02-17 14:31:37 +0100

    ⚠️add prints

--- source/Socket.cpp
+++ source/Socket.cpp
@@ -34,7 +34,7 @@ std::string     Socket::read() const {
     int size;
 
     size = ::read(this->_socket, buffer, MAX_BUFF_SIZE);
-    std::cout << "read " << size << std::endl;
+    std::cout << "read " << size << "[" << buffer << "]" << std::endl;
     (void)size;
     return (std::string(buffer));
 }
